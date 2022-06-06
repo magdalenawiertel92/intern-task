da00206ffc5bb13a2e7f6031fc4cdb32183f3204
Madzia Wiertel <madzia16261@wp.pl>
2022-06-06 11:23:43 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
-import './App.css';
-import Dot from "./Dot";
+import "./App.css"
+import Dot from "./Dot"
 
 function App() {
-    return (
-        <div className="dots-wrapper">
-            {new Array(20).fill('#000000').map((_, i) => (
-                <Dot key={i}/>
-            ))}
-        </div>
-    );
+  return (
+    <div className="dots-wrapper">
+      {new Array(20).fill("#000000").map((_, i) => (
+        <Dot key={i} index={i} />
+      ))}
+    </div>
+  )
 }
 
-export default App;
+export default App
--- src/Dot.js
+++ src/Dot.js
@@ -1,11 +1,47 @@
-import React from 'react';
-import './Dot.css';
+import React, { useState } from "react"
+import "./Dot.css"
 
-const Dot = () => {
-    // TODO: put your code here
-    return (
-        <div className="dot"/>
-    );
-};
+const Dot = ({ index }) => {
+  // TODO: put your code here
 
-export default Dot;
+  const [bgColor, setBgColor] = useState("#000000")
+
+  const handleChangeBackgroundColor = (e, id) => {
+    const selectedDot = e.target
+    const dots = document.querySelectorAll(".dot")
+    dots.forEach(dot => {
+      dot.innerHTML = ""
+      dot.style.backgroundColor = "#000000"
+    })
+    setBgColor("#" + Math.floor(Math.random() * 16777215).toString(16))
+    selectedDot.innerHTML = id
+  }
+
+  const handleHoverOnDot = e => {
+    const hoveredDot = e.target
+    if (hoveredDot.style.backgroundColor === "rgb(0, 0, 0)") {
+      setBgColor("#e8353f")
+    }
+  }
+
+  const handleMouseLeave = e => {
+    const leaveDot = e.target
+    if (leaveDot.style.backgroundColor === "rgb(232, 53, 63)") {
+      setBgColor("#000000")
+    }
+  }
+
+  return (
+    <div
+      className="dot"
+      style={{ backgroundColor: bgColor }}
+      onClick={e => {
+        handleChangeBackgroundColor(e, index)
+      }}
+      onMouseOver={handleHoverOnDot}
+      onMouseLeave={handleMouseLeave}
+    />
+  )
+}
+
+export default Dot
